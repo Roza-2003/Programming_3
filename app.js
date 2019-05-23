979abc39b034fe574520be848c358563100dcc8d
Your Name <you@example.com>
2019-05-23 18:07:27 +0400

    pox

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var fs = require('fs');
 
 app.use(express.static("."));
 
@@ -17,9 +18,9 @@ server.listen(3000, function () {
 
 Weather = "Summer";
 Weatherinit = 1;
-// xotClec = 0;
-// xotakerCnvec = 0;
-// gishatichCnvec = 0;
+xotClec = 0;
+xotakerCnvec = 0;
+gishatichCnvec = 0;
 // ancrevEkav = 0;
 // cyunEkav = 0;
 
@@ -67,66 +68,20 @@ for (var y = 0; y < matrix.length; y++) {
 
     if (matrix[y][x] == 1) {
       grassArr.push(new Grass(x, y, 1));
-      // grass=0 xotClec++
+      xotClec++;
     }
     else if (matrix[y][x] == 2) {
       grasseaterArr.push(new GrassEater(x, y, 2));
+      xotakerCnvec++;
     }
     else if (matrix[y][x] == 3) {
       predatorArr.push(new Predator(x, y, 3));
+      gishatichCnvec++;
     }
   }
 }
-// io.on('connection', function (socket) {
-//   socket.emit("first matrix", matrix);
-
-//   // socket.emit("exanak", weather);
-
-//   setInterval(function () {
-
-//     // takter++;
-//     // console.log(takter);
-
-//     for (var y = 0; y < matrix.length; y++) {
-//       for (var x = 0; x < matrix[y].length; x++) {
-
-//         if (matrix[y][x].index == 1) {
-//           matrix[y][x].mul(matrix);
-//         }
-//         else if (matrix[y][x].index == 2) {
-//           matrix[y][x].eat(matrix);
-//         }
-//         else if (matrix[y][x].index == 3) {
-//           matrix[y][x].eat(matrix);
-//         }
-//         else if (matrix[y][x].index == 4) {
-//           matrix[y][x].eat(matrix);
-//         }
-//         else if (matrix[y][x].index == 5) {
-//           matrix[y][x].eat(matrix);
-//         }
-//       }
-
-// if (takter % 200 == 0) {
-//   weather = "summer";
-//   andzrevStexcel(x, y,index);
-//   console.log(index);
-
-//   if (matrix[y][x].index == 4)
-//     matrix[y][x] = 0;
-// }
-// else if (takter % 200 == 100) {
-//   weather = "winter";
-
-//   dzyunStexcel(x, y);
-
-//   if (matrix[y][x].index == 5)
-//     matrix[y][x] = 0;
-// }
-// }
 
 function drawserever() {
-
   for (var i in grassArr) {
     grassArr[i].mul();
   }
@@ -168,32 +123,6 @@ io.on('connection', function (socket) {
     var x = arr[0];
     var y = arr[1];
 
-    if (matrix[y][x] == 1) {
-      for (var i in grassArr) {
-        if (y == grassArr[i].y && x == grassArr[i].x) {
-          grassArr.splice(i, 1);
-          break;
-        }
-      }
-    }
-    else if (matrix[y][x] == 2) {
-      for (var i in grasseaterArr) {
-        if (y == grasseaterArr[i].y && x == grasseaterArr[i].x) {
-          grasseaterArr.splice(i, 1);
-          break;
-        }
-      }
-    }
-    else if (matrix[y][x] == 3) {
-      for (var i in predatorArr) {
-        if (y == predatorArr[i].y && x == predatorArr[i].x) {
-          predatorArr.splice(i, 1);
-          break;
-        }
-      }
-    }
-    matrix[y][x] = 0;
-
     var kordinatner = [
       [x - 1, y - 1],
       [x, y - 1],
@@ -204,13 +133,39 @@ io.on('connection', function (socket) {
       [x, y + 1],
       [x + 1, y + 1]
     ];
+    if (matrix[x][y] == 1) {
+      for (var i in grassArr) {
+        if (y == grassArr[i].y && x == grassArr[i].x) {
+          grassArr.splice(i, 1);
+          break;
+        }
+      }
+    }
+    else if (matrix[x][y] == 2) {
+      for (var i in grasseaterArr) {
+        if (y == grasseaterArr[i].y && x == grasseaterArr[i].x) {
+          grasseaterArr.splice(i, 1);
+          break;
+        }
+      }
+    }
+    else if (matrix[x][y] == 3) {
+      for (var i in predatorArr) {
+        if (y == predatorArr[i].y && x == predatorArr[i].x) {
+          predatorArr.splice(i, 1);
+          break;
+        }
+      }
+    }
+    matrix[y][x] = 0;
+
     for (var i in kordinatner) {
       var kordX = kordinatner[i][0];
       var kordY = kordinatner[i][1];
       if (kordX >= 0 && kordX < matrix[0].length && kordY >= 1 && kordY < matrix[1].length) {
         if (matrix[kordY][kordX] == 1) {
           for (var i in grassArr) {
-            if (kordY == grassArr[i].kordY && kordX == grassArr[i].kordX) {
+            if (kordY == grassArr[i].y && kordX == grassArr[i].x) {
               grassArr.splice(i, 1);
               break;
             }
@@ -218,7 +173,7 @@ io.on('connection', function (socket) {
         }
         else if (matrix[kordY][kordX] == 2) {
           for (var i in grasseaterArr) {
-            if (kordY == grasseaterArr[i].kordY && kordX == grasseaterArr[i].kordX) {
+            if (kordY == grasseaterArr[i].y && kordX == grasseaterArr[i].x) {
               grasseaterArr.splice(i, 1);
               break;
             }
@@ -226,7 +181,7 @@ io.on('connection', function (socket) {
         }
         else if (matrix[kordY][kordX] == 3) {
           for (var i in predatorArr) {
-            if (kordY == predatorArr[i].kordY && kordX == predatorArr[i].kordX) {
+            if (kordY == predatorArr[i].y && kordX == predatorArr[i].x) {
               predatorArr.splice(i, 1);
               break;
             }
@@ -236,7 +191,28 @@ io.on('connection', function (socket) {
     }
     matrix[kordY][kordX] = 0;
   });
-  io.sockets.emit("matrix", matrix);
+  socket.on("armagedon", function(){
+    for(var y = 0; y<matrix.length; y++){
+      for(var x = 0; x<matrix[y].length; x++){
+        matrix[y][x]=6;
+      }
+    }
+    grassArr.length = 0;
+    grasseaterArr.length = 0;
+    predatorArr.length = 0;
+
+    io.sockets.emit("matrix", matrix);
+  })
 });
+
+
+var obj = { "info": [] };
+
+function main() {
+  var file = "Statistics.json";
+  obj.info.push({ "cnvac xoteri qanak": xotClec, "cnvec xotaker": xotakerCnvec, "cnvec gishatich": gishatichCnvec });
+  fs.writeFileSync(file, JSON.stringify(obj, null, 3));
+}
 setInterval(drawserever, 1000);
-setInterval(draw_wheater, 3000);
+setInterval(draw_wheater, 5000);
+setInterval(main, 2000);
--- public/script.js
+++ public/script.js
@@ -11,33 +11,12 @@ socket.on("exanak", function (w) {
 function setup() {
   createCanvas(20 * side, 20 * side);
   background('#FAFDAD');
-
-  // frameRate(0);
-  // background('#FAFDAD');
-
-  // socket = io();
-
-  // socket.on("first matrix", function (mtx) {
-  //   matrix = mtx;
-  //   createCanvas(matrix[0].length * side + 1, matrix.length * side + 1);
-  //   console.log(matrix);
-
-  // socket.on("exanak", function (wth) {
-  //   weather = wth;
-  // });
-
-  // socket.on("redraw matrix", function(mtx){
-  //   matrix = mtx;
-  //   redraw();
-  // });
-  // });
 }
-// noLoop();
-// }
+
 function drawWeather(w) {
   var p = document.getElementById("seasons");
   var weather = w;
-  console.log(weather);
+  //console.log(weather);
   if (weather == "Summer") {
     p.innerText = "Summer";
   }
@@ -53,27 +32,27 @@ function drawWeather(w) {
 }
 function drawMatrix(matrix) {
   background('#33FFFF');
- // console.log(matrix);
+  // console.log(matrix);
   for (var y = 0; y < matrix.length; y++) {
     for (var x = 0; x < matrix[y].length; x++) {
 
       if (matrix[y][x] == 0) {
-        fill("#6cb1e2");
+        fill("#acacac");
         rect(x * side, y * side, side, side);
       }
       else if (matrix[y][x] == 1) {
         if (wetherclient == "Summer") {
-          fill("#6bb555");
+          fill("green");
         }
         else if (wetherclient != "Summer") {
-          fill("#f9f77c");
+          fill("#e25802");
         }
         rect(x * side, y * side, side, side);
       }
 
       else if (matrix[y][x] == 2) {
         if (wetherclient == "Winter") {
-          fill("#696968");
+          fill("#b1e2e0");
         }
         else if (wetherclient != "Winter") {
           fill("Yellow");
@@ -84,65 +63,23 @@ function drawMatrix(matrix) {
         fill("red");
         rect(x * side, y * side, side, side);
       }
+      else if (matrix[y][x] == 4) {
+        fill("blue");
+        rect(x * side, y * side, side, side);
+      }
     }
   }
 }
 socket.on("matrix", drawMatrix);
 socket.on("exanak", drawWeather);
 
-function mousePressed(){
+function mousePressed() {
   var x = Math.floor(mouseX / side);
   var y = Math.floor(mouseY / side);
-  arr = [x,y];
- // console.log(arr);
-  socket.emit("Sxmvec",arr)
+  arr = [x, y];
+  // console.log(arr);
+  socket.emit("Sxmvec", arr)
 }
-
-// var dzyunKa = false;
-
-// function draw() {
-//   socket.on("exanak", function (wth) {
-//     weather = wth;
-//   });
-//   console.log(weather);
-
-//   if (weather == "winter") {
-//     background("#acacac");
-//   }
-//   else if (weather == "summer") {
-//     background("#FAFDAD");
-//   }
-//   for (var i = 0; i < matrix.length; i++) {
-//     for (var j = 0; j < matrix[i].length; j++) {
-//       if (matrix[i][j].index == 1) {
-//         fill("green");
-//         rect(j * side, i * side, side, side);
-//       }
-//       else if (matrix[i][j].index == 2) {
-//         fill("yellow");
-//         rect(j * side, i * side, side, side);
-//       }
-//       else if (matrix[i][j].index == 3) {
-//         fill("red");
-//         rect(j * side, i * side, side, side);
-//       }
-//       else if (weather == "winter") {
-//         if (matrix[i][j].index == 4) {
-//           fill("write");
-//           ellipse(j * side + 10, i * side + 10, side, side);
-//         }
-//       }
-//       else if (weather == "summer") {
-//         if (matrix[i][j].index == 5) {
-//           fill("#84BDF6");
-//           ellipse(j * side + 10, i * side + 10, side, side);
-
-//         }
-//       }
-//     }
-//   }
-  // textSize(30);
-  // fill("black");
-  // text("Game of life");
-  // line(70,80,130,180);
-// }
+function FireButton() {
+  socket.emit("armagedon");
+}
